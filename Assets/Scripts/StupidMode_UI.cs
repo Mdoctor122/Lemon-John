2b1d9316e704cfb986f2dd526097296755987dda
aryafire1 <erthorsen876@gmail.com>
2023-12-08 12:51:41 -0500

    Tried getting the ui to display again, failed

--- Assets/Scripts/StupidMode_UI.cs
+++ Assets/Scripts/StupidMode_UI.cs
@@ -1,22 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class StupidMode_UI : MonoBehaviour
 {
     public GameObject stupidMode;
+    public TextMeshProUGUI textDisplay;
     
     // Start is called before the first frame update
     void Start()
     {
-        stupidMode.SetActive(false);
+        stupidMode.SetActive(false); //this and same one below in if statement was "old me trying to get it to work"
+        textDisplay.gameObject.SetActive(false); //this is me trying to figure out tmpro code also
     }
 
     // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.F)) {
-            stupidMode.SetActive(true);
+            
+            stupidMode.SetActive(true); 
+            textDisplay.gameObject.SetActive(true);
         }
     }
 }
